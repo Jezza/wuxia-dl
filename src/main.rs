fbeab6456e1234cd0a51302bd678315f414b9352
Jezza <jezzadabomb@gmail.com>
2018-10-01 22:51:33 +0200

    Add fancy progress bar. Add a macro to help add more markup variants that Wuxia use. (If it there's a large amount of variation on the expected format, I'll have to think of a better solution, as it stands, there's just three different variants.)

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@
 extern crate epub_builder;
 #[macro_use]
 extern crate error_chain;
+extern crate indicatif;
 extern crate rayon;
 extern crate regex;
 extern crate reqwest;
@@ -13,6 +14,7 @@ use epub_builder::EpubBuilder;
 use epub_builder::EpubContent;
 use epub_builder::ReferenceType;
 use epub_builder::ZipLibrary;
+use indicatif::{ProgressBar, ProgressStyle};
 use rayon::prelude::*;
 use regex::Regex;
 use reqwest::Client;
@@ -24,6 +26,8 @@ use std::fs::{File, remove_file};
 use std::io::Cursor;
 use std::path::Path;
 use url::Url;
+
+type Pages = Vec<EpubContent<Cursor<String>>>;
 
 mod errors {
 	error_chain! {}
@@ -72,26 +76,39 @@ fn run(args: Vec<String>) -> Result<()> {
 
 	let size = info.chapters.len();
 
-	let pages: Vec<EpubContent<Cursor<String>>> = info.chapters
-				  .into_par_iter()
-				  .map(|chapter| {
-					  fetch_chapter_content(&client, chapter, size)
-						  .chain_err(|| "Unable to fetch chapter content")
-						  .unwrap()
-				  })
-				  .collect();
+	let bar: ProgressBar = ProgressBar::new(size as u64);
+	bar.set_style(ProgressStyle::default_bar()
+		.template("[{prefix}] [{bar:40}] {pos}/{len} ({eta}) {msg}")
+		.progress_chars("=>-"));
+	bar.set_prefix("FETCH");
+	bar.set_message("");
 
-	let mut index = 0;
-	for page in pages {
-		builder.add_content(page)
-			   .chain_err(|| format!("Unable to add page: {}/{}", index, size))?;
-		index += 1;
-	}
+	let pages: Pages = info.chapters
+						   .into_par_iter()
+						   .map(|chapter| {
+							   fetch_chapter_content(&client, chapter, &bar)
+								   .chain_err(|| "Unable to fetch chapter content")
+								   .unwrap()
+						   })
+						   .collect();
 
 	let path = format!("{}.epub", info.title);
 	let path = Path::new(&path);
+
+	bar.set_prefix("EPUB");
+	bar.set_message("");
+	bar.set_position(0);
+
+	for page in pages {
+		bar.inc(1);
+		builder.add_content(page)
+			   .chain_err(|| format!("Unable to add page."))?;
+	}
+
+	bar.finish();
+
 	if path.exists() {
-		println!("File (\"{}\") already exists. Deleting...", path.display());
+		println!("File (\"{}\") already exists. Deleting previous epub...", path.display());
 		remove_file(path)
 			.chain_err(|| format!("Failed to remove previous file: \"{}\"", path.display()))?;
 	}
@@ -153,33 +170,44 @@ fn fetch_book_info(client: &Client, url: Url) -> Result<BookInfo> {
 		chapters,
 	};
 
-	println!("Found \"{}\" with {} chapters at \"{}\"", info.title, info.chapters.len(), url);
+	println!("Found \"{}\" with {} chapters.", info.title, info.chapters.len());
 
 	Ok(info)
 }
 
-fn fetch_chapter_content(client: &Client, chapter: Chapter, size: usize) -> Result<EpubContent<Cursor<String>>> {
+macro_rules! try_with {
+    ($content:ident, $doc:ident, $target:expr) => (
+		for node in $doc.find($target) {
+			let text = node.text();
+			if text.len() == 0 {
+				continue;
+			}
+			$content.push_str(&text);
+			$content.push_str(&"<br><br> ");
+		}
+    )
+}
+
+fn fetch_chapter_content(client: &Client, chapter: Chapter, bar: &ProgressBar) -> Result<EpubContent<Cursor<String>>> {
 	let req = client.get(chapter.link)
 					.build()
 					.chain_err(|| "Unable to construct chapter request.")?;
 
-	println!("Fetching {}/{} :: \"{}\".", chapter.index, size, chapter.title);
+	bar.inc(1);
+	bar.set_message(&chapter.title);
 
 	let mut res = client.execute(req)
 						.chain_err(|| "Unable to send chapter request.")?;
 
-	let doc = Document::from_read(&mut res)
+	let doc: Document = Document::from_read(&mut res)
 		.chain_err(|| "Invalid content from request")?;
 
 	let mut content = String::new();
-	for node in doc.find(Class("fr-view").descendant(Name("span"))) {
-		content.push_str(&node.text());
-		content.push_str(&"<br><br> ");
-	}
+	try_with!(content, doc, Class("innerContent").and(Class("fr-view")).descendant(Name("p")));
 	if content.len() == 0 {
-		for node in doc.find(Class("fr-view").child(Name("p"))) {
-			content.push_str(&node.text());
-			content.push_str(&"<br><br> ");
+		try_with!(content, doc, Class("fr-view").child(Name("p")));
+		if content.len() == 0 {
+			try_with!(content, doc, Class("fr-view").descendant(Name("span")));
 		}
 	}
 	if content.len() == 0 {
